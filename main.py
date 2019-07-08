f84cd4694e076b5e4f9a8c691a8851f51098b16c
NAD777 <nehaev07.07.2003@gmail.com>
2019-07-08 10:56:42 +0300

    add score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#birth commit :)
 import pygame
 from random import randint
+import time
+
+
 class Part_tail:
     def __init__(self, x, y, pos):
         self.x = x
@@ -9,10 +11,10 @@ class Part_tail:
         self.width = 10
         self.height = 10
         self.speed = 10
-    
-    def set_x(self, x): 
+
+    def set_x(self, x):
         self.x = x
-    
+
     def set_y(self, y):
         self.y = y
 
@@ -27,48 +29,50 @@ class Part_tail:
 
     def get_x(self):
         return self.x
-    
+
     def get_y(self):
         return self.y
-    
+
     def get_width(self):
         return self.width
-    
+
     def get_height(self):
         return self.height
-    
+
     def __str__(self):
         return f'x: {self.x}, y:{self.y}'
-    
+
+
 class Fruit:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.width = 10
         self.height = 10
-    
+
     def check(self, x, y):
         if self.x == x and self.y == y:
             return True
         else:
             return False
-    
+
     def __call__(self, x, y):
         self.x = x
         self.y = y
 
     def get_x(self):
         return self.x
-    
+
     def get_y(self):
         return self.y
 
     def get_height(self):
         return self.height
-    
+
     def get_width(self):
         return self.width
-        self.y =  y
+        self.y = y
+
 
 class Snake:
     def __init__(self, x, y):
@@ -80,13 +84,13 @@ class Snake:
         self.height = 10
         self.speed = 10
         self.tail = [Part_tail(self.x_head, self.y_head, self.cur_pos)]
-    
+
     def add_piece(self):
         pos_x, pos_y = self.poses[self.cur_pos]
         self.x_head = self.x_head + self.speed * pos_x
         self.y_head = self.y_head + self.speed * pos_y
         self.tail.insert(0, Part_tail(self.x_head, self.y_head, self.cur_pos))
-    
+
     def move(self):
         self.add_piece()
         self.tail.pop()
@@ -98,19 +102,26 @@ class Snake:
         self.cur_pos = self.cur_pos - 1 if self.cur_pos - 1 != -1 else 3
 
     def rot_right(self):
-        self.cur_pos = self.cur_pos + 1 if self.cur_pos + 1 != 4 else 0  
-    
+        self.cur_pos = self.cur_pos + 1 if self.cur_pos + 1 != 4 else 0
+
     def print_tail(self):
         # print(self.get_snake())
         for el in self.get_snake():
             print(el)
+
     def is_alive(self):
+        for el in self.tail[1:]:
+            if self.x_head == el.get_x() and self.y_head == el.get_y():
+                return False
         if self.x_head == WIDTH or self.y_head == HEIGHT or self.x_head == -10 or self.y_head == -10:
             return False
-        else:
-            return True
+        return True
+
+
 WIDTH = 720
 HEIGHT = 460
+
+
 class Board:
     def __init__(self, snake):
         pygame.init()
@@ -119,8 +130,16 @@ class Board:
         self.clock = pygame.time.Clock()
         self.snake = snake
         self.score = 0
-        self.fruit = Fruit(randint(0, WIDTH - 1) // 10 * 10, randint(0, HEIGHT - 1) // 10 * 10)
+        self.fruit = Fruit(randint(0, WIDTH - 1) // 10 * 10,
+                           randint(0, HEIGHT - 1) // 10 * 10)
         self.fps_controller = pygame.time.Clock()
+
+    def show_score(self):
+        s_font = pygame.font.SysFont('monaco', 24)
+        s_surf = s_font.render('Score: {0}'.format(self.score), True, (0, 0, 0))
+        s_rect = s_surf.get_rect()
+        s_rect.midtop = (50, 10)
+        self.screen.blit(s_surf, s_rect)
 
     def main_action(self):
         while True:
@@ -129,8 +148,10 @@ class Board:
             self.draw_fruit()
             self.draw_snake()
             self.pudge()
+            self.show_score()
             # self.snake.next()
             if not self.snake.is_alive():
+                print(self.score)
                 exit()
             # pygame.display.update()
             pygame.display.flip()
@@ -139,7 +160,8 @@ class Board:
     def pudge(self):
         head = self.snake.get_snake()[0]
         if self.fruit.check(head.get_x(), head.get_y()):
-            self.fruit(randint(0, WIDTH - 1) // 10 * 10, randint(0, HEIGHT - 1) // 10 * 10)
+            self.fruit(randint(0, WIDTH - 1) // 10 * 10,
+                       randint(0, HEIGHT - 1) // 10 * 10)
             self.score += 1
             self.snake.add_piece()
         else:
@@ -147,13 +169,12 @@ class Board:
 
     def draw_fruit(self):
         pygame.draw.rect(self.screen, (255, 0, 0), (self.fruit.get_x(), self.fruit.get_y(), self.fruit.get_width(), self.fruit.get_height()))
-    
+
     def fill_brd(self):
-        self.screen.fill((255,255,255))
+        self.screen.fill((255, 255, 255))
 
     def draw_snake(self):
         arr = self.snake.get_snake()
-        # pygame.draw.rect(self.screen, (255, 165, 0), (arr[0].get_x(), arr[0].get_y(), arr[0].get_width(), arr[0].get_height()))
         for el in arr:
             pygame.draw.rect(self.screen, (0, 165, 0), (el.get_x(), el.get_y(), el.get_width(), el.get_height()))
 
@@ -166,12 +187,12 @@ class Board:
                 if event.key == pygame.K_a:
                     self.snake.rot_left()
                 if event.key == pygame.K_d:
-                    self.snake.rot_right()                    
+                    self.snake.rot_right()
                 if event.key == pygame.K_l:
                     print(self.snake.len)
                 if event.key == pygame.K_p:
                     self.snake.print_tail()
-                    
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             pygame.quit()
@@ -182,8 +203,7 @@ class Board:
         # if keys[pygame.K_f]:
         #     self.snake.add_piece()
         # if keys[pygame.K_m]:
-        
-    
+
 
 snk = Snake(0, 0)
 brd = Board(snk)
